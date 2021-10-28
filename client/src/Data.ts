2ee8fc7608b5e020e2b800c7bc0a6421ef5c0bda
Kim Dong Hun <hunkim98@gmail.com>
2021-10-28 23:40:52 +0900

    Create DTO for making zerowaste questions

---
+++ client/src/Data.ts
@@ -0,0 +1,40 @@
+interface questionDTO {
+  question: string;
+  options: optionsDTO[];
+}
+
+interface optionsDTO {
+  choice: string;
+  effectOnWaste: number[];
+  //example of effectOnWaste: [waste.plastic, waste.plastic, waste.vinyl, waste.paper] = [0,0,1,2]
+}
+
+export enum waste { //enum is for typescript
+  //store waste result in an array
+  plastic = 0,
+  vinyl = 1,
+  paper = 2,
+  rubber = 3,
+  metal = 4,
+  glass = 5,
+}
+
+export const questions: questionDTO[] = [
+  {
+    question: "오늘 아침에 어떤 방법으로 세수하셨나요?",
+    options: [
+      {
+        choice: "비누로 세수했다",
+        effectOnWaste: [waste.vinyl, waste.vinyl, waste.paper],
+      },
+      {
+        choice: "핸드워시로 세수했다",
+        effectOnWaste: [waste.plastic, waste.plastic, waste.plastic],
+      },
+      {
+        choice: "물티슈로 했다",
+        effectOnWaste: [waste.vinyl, waste.paper, waste.paper],
+      },
+    ],
+  },
+];
