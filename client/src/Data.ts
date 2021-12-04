49c60d5eb4267494f77c1a4fbbff546a5d1184e7
eungi817 <smouse0213@naver.com>
2021-12-04 12:56:51 +0900

    start_home_data

--- client/src/Data.ts
+++ client/src/Data.ts
@@ -43,23 +43,23 @@ export const wasteArray = [
 
 export const questions: questionDTO[] = [
   {
-    question: "하루의 시작, 나는 오늘 ________로 세수했다.",
+    question: "하루의 시작, 나는 오늘 _______로 세수했다.",
     options: [
       {
         choice: "물",
-        effectOnWaste: { vinyl: 18, paper: 12 },
+        effectOnWaste: {},
       },
       {
         choice: "비누",
-        effectOnWaste: { vinyl: 18, paper: 22 },
+        effectOnWaste: { vinyl: 6, paper: 5 },
       },
       {
         choice: "물티슈",
-        effectOnWaste: { vinyl: 8, microplastic: 22 },
+        effectOnWaste: { vinyl: 5, microplastic: 45 },
       },
       {
         choice: "폼클렌저",
-        effectOnWaste: { plastic: 28 },
+        effectOnWaste: { plastic: 8 },
       },
     ],
   },
@@ -68,15 +68,15 @@ export const questions: questionDTO[] = [
     options: [
       {
         choice: "배달음식을 시킨다",
-        effectOnWaste: { vinyl: 18, paper: 12 },
+        effectOnWaste: { vinyl: 6, plastic: 9 },
       },
       {
-        choice: "간편조리식품(햇반, 컵라면, 김밥 등...)을 먹는다",
-        effectOnWaste: { vinyl: 7, paper: 7, plastic: 7 },
+        choice: "간편조리식품(햇반, 컵라면, 김밥 등)을 먹는다",
+        effectOnWaste: { vinyl: 5, paper: 3, plastic: 9 },
       },
       {
         choice: "밀키트 등으로 간단하게 조리한다",
-        effectOnWaste: { vinyl: 13, plastic: 17 },
+        effectOnWaste: { vinyl: 6, plastic: 9 },
       },
       {
         choice: "직접 요리해먹는다",
@@ -86,85 +86,63 @@ export const questions: questionDTO[] = [
   },
   {
     question:
-      "외출하는 길에 보니 택배가 와있다! 요즘 배송받는 물건들의 포장에는 _________가 가장 많다.",
+      "외출하는 길에 보니 택배가 와있다! 요즘 배송받는 물건들의 포장에는 _______가 가장 많다.",
     options: [
       {
         choice: "아이스팩",
-        effectOnWaste: { microplastic: 40 },
+        effectOnWaste: { microplastic: 30 },
       },
       {
         choice: "종이상자",
-        effectOnWaste: { paper: 29 },
+        effectOnWaste: { paper: 4 },
       },
       {
         choice: "비닐",
-        effectOnWaste: { vinyl: 29 },
+        effectOnWaste: { vinyl: 8 },
       },
       {
         choice: "아이스박스",
-        effectOnWaste: { styrofoam: 32 },
+        effectOnWaste: { styrofoam: 48 },
       },
     ],
   },
   {
     question:
-      "요즘은 날씨 탓에 물을 많이 마시게 된다. 나는 밖에서 물을 마실 때, __________. ",
+      "날씨 탓에 물을 많이 마시는 요즘, 나는 밖에서 물을 마실 때, __________. ",
     options: [
       {
         choice: "생수를 사 마신다",
-        effectOnWaste: { plastic: 31 },
+        effectOnWaste: { plastic: 8 },
       },
       {
         choice: "생수병을 재사용한다 ",
-        effectOnWaste: { plastic: 16 },
+        effectOnWaste: { plastic: 6 },
       },
       {
-        choice: "종이컵",
-        effectOnWaste: { paper: 31 },
+        choice: "종이컵을 사용한다",
+        effectOnWaste: { paper: 4 },
       },
       {
-        choice: "개인 물병에 마신다 ",
+        choice: "개인 물병에 마신다",
         effectOnWaste: {},
       },
     ],
   },
   {
     question:
-      "잠깐 화장실을 들렀다. 나는 화장실에서 손을 씻으면 보통 ___________.",
+      "잠깐 들른 카페, 테이크아웃으로 주문할 때 나는 주로 _______를 마신다.",
     options: [
       {
-        choice: "자연건조 시킨다",
-        effectOnWaste: {},
-      },
-      {
-        choice: "옷에 닦는다",
-        effectOnWaste: {},
-      },
-      {
-        choice: "티슈에 닦는다",
-        effectOnWaste: { paper: 6 },
-      },
-      {
-        choice: "손 건조기에 말린다",
-        effectOnWaste: {},
-      },
-    ],
-  },
-  {
-    question:
-      "시간이 떠서 잠깐 들어온 카페. 테이크아웃으로 주문할 때 나는 주로 _______를 마신다.",
-    options: [
-      {
-        choice: "따뜻한 커피",
-        effectOnWaste: { paper: 17 },
+        choice: "따뜻한 음료",
+        effectOnWaste: { paper: 4 },
       },
       {
         choice: "에이드나 아이스아메리카노",
-        effectOnWaste: { plastic: 17 },
+        effectOnWaste: { plastic: 8 },
       },
       {
         choice: "병에 든 탄산음료",
-        effectOnWaste: { articles: 17 },
+        effectOnWaste: { articles: 24 },
       },
       {
         choice: "(사실 별로 안 마신다)",
@@ -174,33 +152,55 @@ export const questions: questionDTO[] = [
   },
   {
     question:
-      "집 가는 길에 들른 마트. 저기 라면코너가 보인다. 내가 가장 자주 먹는 라면은_____이다.",
+      "마트에 왔다. 식품코너에서 나는 _______을 가장 자주 구매한다.",
     options: [
       {
-        choice: "봉지 국물라면",
-        effectOnWaste: { vinyl: 13 },
+        choice: "참치, 꽁치 통조림",
+        effectOnWaste: { articles: 21 },
       },
       {
-        choice: "봉지 볶음라면",
-        effectOnWaste: { vinyl: 13 },
+        choice: "햄, 닭가슴살 통조림",
+        effectOnWaste: { articles: 21 },
       },
       {
-        choice: "국물 있는 컵라면",
-        effectOnWaste: { styrofoam: 17 },
+        choice: "카레, 짜장 등 레토르트 소스류",
+        effectOnWaste: { vinyl: 5 },
       },
       {
-        choice: "볶음 컵라면",
-        effectOnWaste: { paper: 17 },
+        choice: "간편조리 국, 찌개류",
+        effectOnWaste: { vinyl: 5 },
       },
     ],
   },
   {
     question:
-      "카운터에서 물건 계산하는 중... 나는 계산이 끝난 물건들을 주섬주섬 ________.",
+      "저기 라면코너도 보인다! 내가 가장 자주 먹는 라면은 ____ _______이다.",
+    options: [
+      {
+        choice: "봉지 국물라면",
+        effectOnWaste: { vinyl: 6 },
+      },
+      {
+        choice: "봉지 볶음라면",
+        effectOnWaste: { vinyl: 6 },
+      },
+      {
+        choice: "국물 있는 컵라면",
+        effectOnWaste: { styrofoam: 41 },
+      },
+      {
+        choice: "볶음 컵라면",
+        effectOnWaste: { paper: 5 },
+      },
+    ],
+  },
+  {
+    question:
+      "카운터에서 물건 계산하는 중... 나는 계산이 끝난 물건들을 주섬주섬 _______.",
     options: [
       {
         choice: "비닐봉투에 담는다",
-        effectOnWaste: { vinyl: 9 },
+        effectOnWaste: { vinyl: 4 },
       },
       {
         choice: "장바구니에 담는다",
@@ -208,7 +208,7 @@ export const questions: questionDTO[] = [
       },
       {
         choice: "종이봉투에 담는다",
-        effectOnWaste: { paper: 9 },
+        effectOnWaste: { paper: 4 },
       },
       {
         choice: "손에 들고 간다",
@@ -218,19 +218,19 @@ export const questions: questionDTO[] = [
   },
   {
     question:
-      "할 일이 산더미... 결국 몇 시간 못 잤다. 밤을 샌 다음날 나는 __________.",
+      "할 일이 산더미... 밤을 샌 다음날 나는 편의점에서 __________.",
     options: [
       {
-        choice: "레*불, 몬스* 고카페인 에너지 드링크를 마신다",
-        effectOnWaste: { articles: 21 },
+        choice: "레*불, 몬스* 고카페인 에너지 드링크를 산다",
+        effectOnWaste: { articles: 7 },
       },
       {
-        choice: "정석으로 샷추가 한 커피를 마신다 ",
-        effectOnWaste: { plastic: 21 },
+        choice: "캔커피를 산다",
+        effectOnWaste: { articles: 7 },
       },
       {
-        choice: "박*스같은 비타민 음료를 마신다",
-        effectOnWaste: { articles: 21 },
+        choice: "박*스같은 비타민 음료를 산다",
+        effectOnWaste: { articles: 7 },
       },
       {
         choice: "카페인 없이 버틴다(졸기도 한다)",
